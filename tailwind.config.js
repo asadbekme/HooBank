9acbe85e183281900b4c81e9ba8e1a4315478846
Асадбек* <asadbekme2002@gmail.com>
2023-01-29 23:17:40 +0500

    Tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00040f',
+        secondary: '#00f6ff',
+        lightWhite: 'rgba(255, 255, 255, 0.7)',
+        lightBlue: 'rgba(9, 151, 124, 0.1)'
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif']
+      },
+    },
+    screens: {
+      'xs': '480px',
+      'sm': '620px',
+      'md': '768px',
+      'lg': '1060px',
+      'xl': '1200px',
+      '2xl': '1700px'
+    }
+  },
+  plugins: [],
+}
