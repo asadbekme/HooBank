4a2a121da160c627950c8473ebe095073d2e76de
Асадбек* <asadbekme2002@gmail.com>
2023-01-30 16:14:24 +0500

    Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { navigationLinks } from "../utils/constants";
 import { styles } from "../utils/styles";
-import { logo } from "../assets";
+import { close, logo, menu } from "../assets";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [active, setActive] = useState("home");
+
+  const toggleHandler = () => {
+    setToggleMenu((prevState) => !prevState);
+  };
+  const activeHandler = (id) => setActive(id);
+
   return (
     <div className={`w-full py-6 ${styles.flexBetween}`}>
       {/* Logo */}
@@ -17,14 +26,47 @@ const Navbar = () => {
         {navigationLinks.map((link, idx) => (
           <li
             key={link.id}
-            className={`font-montserrat font-normal cursor-pointer text-[16px] text-white ${
-              navigationLinks.length === idx ? "mr-0" : "mr-10"
-            } hover:text-lightWhite transition-all duration-500`}
+            className={`font-montserrat font-normal cursor-pointer text-[16px] text-white 
+            ${navigationLinks.length === idx ? "mr-0" : "mr-10"} 
+            ${active === link.id ? "text-white" : "text-lightWhite"} 
+            hover:text-white transition-all duration-500`}
+            onClick={() => activeHandler(link.id)}
           >
             {link.title}
           </li>
         ))}
       </ul>
+
+      {/* Menu button */}
+      <div className={`md:hidden flex flex-1 justify-end items-center`}>
+        <img
+          src={toggleMenu ? close : menu}
+          alt="nav"
+          className="w-[30px] h-[30px] object-contain cursor-pointer"
+          onClick={toggleHandler}
+        />
+
+        <div
+          className={`${
+            !toggleMenu ? "hidden" : "flex"
+          } p-6 absolute top-20 right-0 left-0 w-full sidebar bg-black-gradient`}
+        >
+          <ul className={`${styles.flexCenter} flex-1 list-none`}>
+            {navigationLinks.map((link, idx) => (
+              <li
+                key={link.id}
+                className={`font-montserrat font-normal cursor-pointer text-[16px] text-white 
+                ${navigationLinks.length === idx ? "mr-0" : "mr-10"} 
+                ${active === link.id ? "text-white" : "text-lightWhite"} 
+                hover:text-white transition-all duration-500`}
+                onClick={() => activeHandler(link.id)}
+              >
+                {link.title}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
